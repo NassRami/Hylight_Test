d5a11bcf0868dfb03469789c28e33fcd11d14610
Nassim <Nassim.rami99@gmail.com>
2026-08-31 00:31:04 +0200

    UART Control function developped but should fix the crc16

--- lib/UART_Control/UART_Control.c
+++ lib/UART_Control/UART_Control.c
@@ -5,6 +5,7 @@
 static bool uart_frame_state = false; // Flag to indicate if a complete UART frame has been received
 static uint8_t uart_rx_buffer[8u]; // Buffer to store received UART data
 static uint8_t uart_buffer_index; // Index to keep track of the position in the buffer
+static uint8_t rx_byte = 0U;
 
 void UART_Init(void)
 {
@@ -17,14 +18,14 @@ void UART_Init(void)
      */
     (void)HAL_UART_Receive_IT(
         &huart3,
-        &uart_rx_byte,
+        &rx_byte,
         1U
     );
 }
 void HAL_UART_RxCpltCallback(
     UART_HandleTypeDef *huart)
 {
-        if (uart_rx_byte == 0x00U) // Check for end of frame (0x00)
+        if (rx_byte == 0x00U) // Check for end of frame (0x00)
         {
                 uart_frame_state = true; /*Don't Copy the 0x00 to the buffer,
                                           just set the frame state to true */
@@ -34,7 +35,7 @@ void HAL_UART_RxCpltCallback(
             // Store the received byte in the buffer 
             if (uart_buffer_index < 8U)
             {
-                uart_rx_buffer[uart_buffer_index] = uart_rx_byte;
+                uart_rx_buffer[uart_buffer_index] = rx_byte;
                 uart_buffer_index++;
             }
             else
@@ -45,8 +46,59 @@ void HAL_UART_RxCpltCallback(
 
         (void)HAL_UART_Receive_IT( // Restart reception of one byte
             &huart3,
-            &uart_rx_byte,
+            &rx_byte,
             1U
         );
+
+}
+bool UART_COBSDecode(uint8_t *input,//Rx_buff
+                     uint8_t *output,//Buff after decoding
+                      uint8_t length)//Length of the input buffer
+{
+    uint8_t write_index = 0U;
+    uint8_t read_index = 0U;
+    uint8_t COBS_code = 0U;
+
+    while(read_index < length)
+    {
+        COBS_code = input[read_index];//by pass the COBS code
+        read_index++;
+
+        if (COBS_code == 0U)// Invalid COBS code 0x00, cannot decode
+        {
+            return false;
+        }
+        //1 byte COBS + 3 bytes of data
+        for (uint8_t i = 1U; i < COBS_code; i++)
+        {
+            output[write_index] = input[read_index];
+            write_index++;
+            read_index++;
+        }
+
+    }
+    return true;
+}
+void UART_Control_Process(void)
+{
+    uint8_t decoded_buffer[8u]; // Buffer to store the decoded data
+    uint16_t received_CRC = 0U; // Variable to store the received CRC value
+    if (!uart_frame_state)// UART Frame is not ready
+    {
+        return;
+    }
+    uart_frame_state = false; // Reset the frame ready flag
+
+    /* Process the received UART frame  */
+    if(!UART_COBSDecode(uart_rx_buffer, decoded_buffer, uart_buffer_index))
+    {
+        // Handle COBS decoding error
+        uart_buffer_index = 0U; // Reset the buffer index
+        return;
+    }
+
+    received_CRC = ((uint16_t) decoded_buffer[1] << 8)| (uint16_t)decoded_buffer[2] ; // Extract the received CRC from the decoded buffer
+
+    /* Calculate the CRC */
 
 }
--- lib/UART_Control/UART_Control.h
+++ lib/UART_Control/UART_Control.h
@@ -1,5 +1,14 @@
 #ifndef __UART_CONTROL_H
 #define __UART_CONTROL_H
 
+#include <stdbool.h>
+#include "stdint.h"
+
+void UART_Init(void);
+bool UART_COBSDecode(uint8_t *input,//Rx_buff
+                     uint8_t *output,//Buff after decoding
+                      uint8_t length);//Length of the input buffer  
+void UART_Control_Process(void);
+
 
 #endif /* __UART_CONTROL_H */
